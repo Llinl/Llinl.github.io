c973ab1e66dec8e11fdeb687492f2019aed856f2
Llinl <manar@comhem.se>
2016-06-16 21:26:47 +0200

    Map is now making the cityscape

--- gameScript.js
+++ gameScript.js
@@ -8,24 +8,13 @@
 //var arrMoneyMakers = [];
 //var arrMmProd = [1,10,100,1000,10000,100000,1000000,10000000];
 //var arrMmCost = [1,10,100,1000,10000,100000,1000000,10000000];
-/*
-var cityMap =   [[1,1,1,0,1,0,1,0,1],
-                 [0,0,0,0,0,0,0,0,1],
-                 [1,0,1,0,1,1,1,0,1],
-                 [0,0,1,0,0,0,0,0,0],
-                 [1,0,1,0,1,1,1,0,1],
-                 [1,0,0,0,0,0,0,0,0],
-                 [1,0,1,0,1,0,1,1,1],
-                 [0,0,1,0,0,0,1,1,1],
-                 [1,0,1,0,1,0,1,1,1]]; */
 
-var panels = ["panelMap", "panelHenchmen", "panelTraining",
-    "panelResearch", "panelFactory", "panelPowers", "panelSlide"];
+//var panels = ["panelMap", "panelHenchmen", "panelTraining",
+//    "panelResearch", "panelFactory", "panelPowers", "panelSlide"];
 
 var small = true;
 
-//map = new Map();
-
+//This will start the program snce onload calls the init after it is done.
 var imageRepository = new function () {
     "use strict";
     console.log("Start image load");
@@ -37,7 +26,6 @@ var imageRepository = new function () {
     this.spriteMap = new Image();
 
     // Ensure all images have loaded before starting the game
-
 
     function imageLoaded() {
         console.log("Image loaded");
@@ -59,31 +47,71 @@ var imageRepository = new function () {
     this.spriteMap.src = "./Resources/Images/SpriteMap.png";
 };
 
+var sprite = function(){
+    this.spritemap = undefined;
+    this.tileNr = 0;
+    this.x = 0;
+    this.y = 0;
+    this.width = 0;
+    this.height = 0;
+
+    return {spritemap:this.spritemap,
+        tileNr:this.tileNr,
+        x:this.x,
+        y:this.y,
+        width:this.width,
+        height:this.height
+    };
+};
+
+var spriteTileMap = function(sm, w, h){
+    var tileWidth = w;
+    var tileHeight = h;
+    var spritemap = sm;
+
+
+    var getSprite = function(nr){
+        this.returnSpr = new sprite();
+        this.returnSpr.spritemap = spritemap;
+        this.returnSpr.tileNr = nr;
+        this.returnSpr.width = tileWidth;
+        this.returnSpr.height = tileHeight;
+        return this.returnSpr;
+    };
+   return{getSprite:getSprite}
+};
+
 var Person = function(startPosX, startPosY){
-    var oldX = 0;
-    var oldY = 0;
-    var newX = 0;
-    var newY = 0;
+    this.sprite = undefined;
+    this.oldX = startPosX;
+    this.oldY = startPosY;
+    this.newX = startPosX;
+    this.newY = startPosY;
     var startX = startPosX;
     var startY = startPosY;
-    var type = 0;
+
+    var setSprite = function(spr){
+        this.sprite = spr;
+    };
 
     var reset = function() {
         console.log("Person Reset");
-        oldX = startPosX;
-        oldY = startPosY;
-        newX = startPosX;
-        newY = startPosY;
+        this.oldX = this.newX;
+        this.oldY = this.newY;
+        this.newX = startX;
+        this.newY = startY;
 
-        return this;
+
     };
     reset();
-    return{startX:startX,startY:startY,
-        oldX:oldX,
-        oldY:oldY,
-        newX:newX,
-        newY:newY,
-        type:type,
+
+    return{sprite:this.sprite,
+        startX:startX,startY:startY,
+        oldX:this.oldX,
+        oldY:this.oldY,
+        newX:this.newX,
+        newY:this.newY,
+        setSprite:setSprite,
         reset:reset};
 };
 
@@ -94,6 +122,17 @@ var Map = function() {
     var tileHeight = 0;
     var tileScale = 0;
     var startPos = {};
+
+    var getNextStep = function(){
+        var rnd = Math.floor(Math.random() * 4);
+        if (rnd === 0) return {X:-1,Y:0};
+        if (rnd === 1) return {X:1,Y:0};
+        if (rnd === 2) return {X:0,Y:-1};
+        if (rnd === 3) return {X:0,Y:1};
+        return{};
+    };
+
+
      if (small) {
          mapWidth = 9;
          mapHeight = 9;
@@ -143,8 +182,25 @@ var Map = function() {
 
           startPos = {X:8,Y:1};
         }
+    var testLimits = function(person){
+        if (person.X < 0) return false;
+        if (person.X > mapWidth-1) return false;
+        if (person.Y < 0) return false;
+        if (person.Y > mapHeight-1)  return false;
+        return backgroundArr[person.Y][person.X] !== 1;
 
-     return {
+    };
+
+    var testEdges = function(person){
+        if (person.newX === 0) return false;
+        if (person.newX === mapWidth-1) return false;
+        if (person.newY === 0) return false;
+        return person.newY !== mapHeight - 1;
+
+    };
+
+
+    return {
          redraw: function () {
              console.log("Map redraw");
              this.background.redraw();
@@ -170,64 +226,45 @@ var Map = function() {
          people: {
              canvas: document.getElementById("people"),
              context : document.getElementById("people").getContext("2d"),
-             citizens:10000,
+             citizens:1,
              population:[],
              init: function(){
              console.log("People init");
+                 var spriteMap = spriteTileMap(imageRepository.spriteMap,16,16);
                  for(var i = 0; i < this.citizens; i++){
                      var p = new Person(startPos.X, startPos.Y);
-                     p.type = 1;
-                     //p.X = 4;
-                     //p.Y = 4;
+                     p.setSprite(spriteMap.getSprite(1));
+
                      this.population.push(p);
                  }
              },
-             testLimits : function(person){
-                 var returnBool = true;
-                 if (person.X < 0)  returnBool = false;
-                 if (person.X > mapWidth-1)  returnBool = false;
-                 if (person.Y < 0)  returnBool = false;
-                 if (person.Y > mapHeight-1)  returnBool = false;
-                 console.log(person);
-                 if (backgroundArr[person.Y][person.X] === 1) returnBool = false;
 
-                 return returnBool;
-             },
              move : function() {
                  console.log("Map People Move");
                  for (var people = 0; people < this.citizens; people++) {
                      var person = this.population[people];
-
-                     if (person.newX === 0 || person.newX === mapWidth-1 ||
-                         person.newY === 0 || person.newY === mapHeight-1) {
-                         this.population[people].oldX = this.population[people].newX;
-                         this.population[people].oldY = this.population[people].newY;
-                         this.population[people].newX = this.population[people].startX;
-                         this.population[people].newY = this.population[people].startY;
-                     }
-                     else {
+                     if (testEdges(person)) {
                          var moved = false;
                          var nextStep = {X: 0, Y: 0};
                          while (moved === false) {
-                             nextStep = {X: 0, Y: 0};
-                             var rnd = Math.floor(Math.random() * 4);
-                             if (rnd === 0) nextStep.X = -1;
-                             if (rnd === 1) nextStep.X = 1;
-                             if (rnd === 2) nextStep.Y = -1;
-                             if (rnd === 3) nextStep.Y = 1;
 
-                             if (this.testLimits({
-                                     X: nextStep.X + this.population[people].newX,
-                                     Y: nextStep.Y + this.population[people].newY
+                             nextStep = getNextStep();
+
+                             if (testLimits({
+                                     X: nextStep.X + person.newX,
+                                     Y: nextStep.Y + person.newY
                                  })) {
                                  moved = true;
                              }
-                             if (nextStep.X + this.population[people].newX === this.population[people].oldX && nextStep.Y + this.population[people].newY === this.population[people].oldY) moved = false;
+                             if (nextStep.X + person.newX === person.oldX && nextStep.Y + person.newY === person.oldY) moved = false;
                          }
                          this.population[people].oldX = this.population[people].newX;
                          this.population[people].oldY = this.population[people].newY;
                          this.population[people].newX += nextStep.X;
                          this.population[people].newY += nextStep.Y;
+                     } else {
+                         this.population[people].reset();
+
                      }
                  }
              },
@@ -235,12 +272,11 @@ var Map = function() {
              redraw : function () {
                  console.log("Map People Redraw");
                  for (var people = 0; people < this.citizens; people++) {
-                     //console.log("oldX = " + this.population[people].oldX + " oldY = " + this.population[people].oldY);
-                     this.context.clearRect(this.population[people].oldX * tileWidth * tileScale, this.population[people].oldY * tileHeight * tileScale, tileWidth * tileScale, tileHeight * tileScale);
+                     var person = this.population[people];
+                     var spr = person.sprite;
+                     this.context.clearRect(person.oldX * tileWidth * tileScale, person.oldY * tileHeight * tileScale, tileWidth * tileScale, tileHeight * tileScale);
 
-                     //console.log("newX = " + this.population[people].newX + " newY = " + this.population[people].newY);
-
-				     this.context.drawImage(imageRepository.spriteMap, this.population[people].type * 16, 0,16 ,16 ,this.population[people].newX * tileWidth * tileScale , this.population[people].newY * tileHeight * tileScale,tileWidth * tileScale, tileHeight * tileScale);
+				     this.context.drawImage(spr.spritemap,spr.tileNr * spr.width, 0,spr.width ,spr.height ,person.newX * tileWidth * tileScale , person.newY * tileHeight * tileScale,tileWidth * tileScale, tileHeight * tileScale);
                  }
              }
          }
@@ -263,14 +299,13 @@ var Map = function() {
 var map = new Map();
 
 function init(){
+
     map.redraw();
     map.people.init();
     map.people.redraw();
 
-    //var myVar = setInterval(movePeople, 1000);
     setInterval(movePeople, 1000);
 
-    //map.player.redraw();
 
     /*
 
@@ -292,9 +327,9 @@ function movePeople(){
 
 function switchSize(){
     var contextb= document.getElementById("background").getContext("2d");
-    contextb.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    contextb.clearRect(0, 0, contextb.canvas.width, contextb.canvas.height);
     var contextp = document.getElementById("people").getContext("2d");
-    contextp.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    contextp.clearRect(0, 0, contextp.canvas.width, contextp.canvas.height);
 
     small = !small;
     map = new Map();
